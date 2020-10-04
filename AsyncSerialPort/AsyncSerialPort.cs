56d382366973a774fbc7cb22917675c7a1297d96
khf05137 <khf05137@nifty.ne.jp>
2020-10-04 14:31:57 +0900

    create AsyncSerialPort

---
+++ AsyncSerialPort/AsyncSerialPort.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.IO.Ports;
+using System.Threading.Tasks;
+
+namespace khf05137.IO.Ports
+{
+    public class AsyncSerialPort : SerialPort, IAsyncSerialPort
+    {
+        public AsyncSerialPort() : base() { }
+        public AsyncSerialPort(IContainer container) : base(container) { }
+        public AsyncSerialPort(string portName) : base(portName) { }
+        public AsyncSerialPort(string portName, int baudRate) : base(portName, baudRate) { }
+        public AsyncSerialPort(string portName, int baudRate, Parity parity) : base(portName, baudRate, parity) { }
+        public AsyncSerialPort(string portName, int baudRate, Parity parity, int dataBits) : base(portName, baudRate, parity, dataBits) { }
+        public AsyncSerialPort(string portName, int baudRate, Parity parity, int dataBits, StopBits stopBits) : base(portName, baudRate, parity, dataBits, stopBits) { }
+
+        public Task<int> ReadAsync(byte[] buffer, int offset, int count) => Task.Run(() => Read(buffer, offset, count));
+        public Task<int> ReadAsync(char[] buffer, int offset, int count) => Task.Run(() => Read(buffer, offset, count));
+        public Task<int> ReadByteAsync() => Task.Run(() => ReadByte());
+        public Task<int> ReadCharAsync() => Task.Run(() => ReadChar());
+        public Task<string> ReadLineAsync() => Task.Run(() => ReadLine());
+        public Task<string> ReadToAsync(string value) => Task.Run(() => ReadTo(value));
+        public Task WriteAsync(byte[] buffer, int offset, int count) => Task.Run(() => Write(buffer, offset, count));
+        public Task WriteAsync(string text) => Task.Run(() => Write(text));
+        public void WriteAsync(char[] buffer, int offset, int count) => Task.Run(() => Write(buffer, offset, count));
+        public void WriteLineAsync(string text) => Task.Run(() => WriteLine(text));
+    }
+}
---
+++ AsyncSerialPort/IAsyncSerialPort.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+
+namespace khf05137.IO.Ports
+{
+    public interface IAsyncSerialPort : ISerialPort
+    {
+        Task<int> ReadAsync(byte[] buffer, int offset, int count);
+        Task<int> ReadAsync(char[] buffer, int offset, int count);
+        Task<int> ReadByteAsync();
+        Task<int> ReadCharAsync();
+        Task<string> ReadLineAsync();
+        Task<string> ReadToAsync(string value);
+        Task WriteAsync(byte[] buffer, int offset, int count);
+        void WriteAsync(char[] buffer, int offset, int count);
+        Task WriteAsync(string text);
+        void WriteLineAsync(string text);
+    }
+}
